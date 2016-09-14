2085f255146e6e6209394b341724d56223910b4e
David Martin <david@martinmacias.com>
2016-09-14 18:53:36 +0200

    added some scripts

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,1 +1,12 @@
 echo "this is master branch"
+
+RELEASE_BRANCH_LIST="$(git branch | grep release-)"
+if [[ -z $RELEASE_BRANCH_LIST ]]; then
+  echo $CURRENT_BRANCH
+else
+  for RELEASE_BRANCH in $RELEASE_BRANCH_LIST
+  do
+    echo $RELEASE_BRANCH
+    break
+  done
+fi
--- update_branches.sh
+++ update_branches.sh
@@ -1,6 +1,17 @@
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ ${BRANCH} == development ]]; then
+CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ ${CURRENT_BRANCH} == development ]]; then
   ./scripts/development.sh
-elif [[ ${BRANCH} == master ]]; then
+elif [[ ${CURRENT_BRANCH} == master ]]; then
   ./scripts/master.sh
+fi
+
+RELEASE_BRANCH_LIST="$(git branch | grep release-)"
+if [[ -z $RELEASE_BRANCH_LIST ]]; then
+  echo $CURRENT_BRANCH
+else
+  for RELEASE_BRANCH in $RELEASE_BRANCH_LIST
+  do
+    echo $RELEASE_BRANCH
+    break
+  done
 fi
