221ce7774fdd5923fe22dac0dfb2d10a20d419e4
David Martin <david@martinmacias.com>
2016-09-14 18:58:11 +0200

    updates

--- update_branches.sh
+++ update_branches.sh
@@ -4,14 +4,3 @@ if [[ ${CURRENT_BRANCH} == development ]]; then
 elif [[ ${CURRENT_BRANCH} == master ]]; then
   ./scripts/master.sh
 fi
-
-RELEASE_BRANCH_LIST="$(git branch | grep release-)"
-if [[ -z $RELEASE_BRANCH_LIST ]]; then
-  echo $CURRENT_BRANCH
-else
-  for RELEASE_BRANCH in $RELEASE_BRANCH_LIST
-  do
-    echo $RELEASE_BRANCH
-    break
-  done
-fi
